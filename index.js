f290283b9fb42451d0796a37d6bfa1e90d9d7ac6
sopykt <sopykt09@gmail.com>
2017-03-23 10:42:15 +0630

    con.query fields

--- index.js
+++ index.js
@@ -52,11 +52,11 @@ con.connect(function(err){
 */
 
 //execute a query against the database table employees
-con.query('SELECT * FROM employees',function(err, rows){
+con.query('SELECT * FROM employees',function(err, fields){
 		if(err) {
 			console.log('error getting query from database');
 		} else {
-			console.log(rows);
+			console.log(fields);
 		}
 	});  
 
